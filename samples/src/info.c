8ffd632b02f3bde271595228a1a9a0e36b235b5c
TomCrypto <thomas.beneteau@yahoo.fr>
2014-06-21 22:58:53 +1200

    Improved info sample

--- samples/src/info.c
+++ samples/src/info.c
@@ -1,13 +1,15 @@
-/* Sample for Ordo - info.c
- * ===============
- *
- * Shows how to discover cipher key lengths. The basic idea is that, given some
- * valid key length K, if key_len(K + 1) == K, then K is the largest key length
- * accepted by the cipher - this allows one to easily iterate through valid key
- * lengths, which is important when the key length is not known in advance.
- *
- * In practice, though, you don't iterate them, and only need to make one call.
-*/
+/*===-- samples/info.c -------------------------------*- PUBLIC -*- H -*-===*/
+/**
+*** @file
+*** @brief Sample
+***
+*** This sample will enumerate all available block ciphers, and will print out
+*** the key lengths they accept. It shows how to use the query API to discover
+*** key lengths, and how to iterate on them - note in practice you do not need
+*** to iterate, since you already know the length of your key material, so you
+*** only need to query the next best key length once.
+**/
+/*===----------------------------------------------------------------------===*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,23 +20,22 @@ int main(void)
 {
     const prim_t *cipher;
 
+    /* The prims_by_type() function gives a zero-terminated array */
     for (cipher = prims_by_type(PRIM_TYPE_BLOCK); *cipher; ++cipher)
     {
         size_t key_len, last_len = (size_t)(-1);
 
         printf("%s:\n", prim_name(*cipher));
-        
+
         for (key_len = block_query(*cipher, KEY_LEN_Q, 0);
-             key_len != last_len; /* One way to iterate them. */
+             key_len != last_len; /* Iterate them e.g. like this. */
              key_len = block_query(*cipher, KEY_LEN_Q, key_len + 1))
         {
-            printf("  * %d bits (%d bytes)\n", (int)key_len * 8,
-                                               (int)key_len);
-            last_len = key_len;
+            printf("  * %d bits\n", (int)key_len * 8);
+            last_len = key_len; /* Until they match */
         }
 
-        if (*(cipher + 1))
-            printf("\n");
+        if (*(cipher + 1)) printf("\n");
     }
 
     return EXIT_SUCCESS;
